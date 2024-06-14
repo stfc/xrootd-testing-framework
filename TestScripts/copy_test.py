1c6ad7ff578163a28f3fef624ba5ffccbd91bb8b
M-A-Demir <m.a.demir1805@gmail.com>
2024-06-14 12:38:01 +0000

    change file names

---
+++ TestScripts/copy_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys   
+sys.path.insert(0, "../TestClasses")
+from BaseTest import BaseTest
+from ReadWriteTest import ReadWriteTest
+
+copyTest = ReadWriteTest()
+copyTest.setup('xrdcp', './tst.txt', 'dteam:/test/tst.txt', '--force')
+
+copyTest.subprocess()
+
